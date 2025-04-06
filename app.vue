0d0245d7f217cdd8696c9c0f3d2a917bcf44005c
Walker <zw37520@gmail.com>
2025-04-06 20:48:37 +0700

    micro-child-nuxt3-home init

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <el-button type="primary">Element Plus 按钮</el-button>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,30 @@
+/*
+ * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Date: 2025-04-01 22:14:11
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2025-04-06 20:41:56
+ * @FilePath: /child-nuxt2-home/nuxt.config.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/test-utils',
+    '@nuxt/ui',
+    '@element-plus/nuxt',
+  ],
+
+  elementPlus: {
+    /** Options */
+    importStyle: 'css',
+    themes: ['dark'],
+  } as Record<string, unknown>,
+});
